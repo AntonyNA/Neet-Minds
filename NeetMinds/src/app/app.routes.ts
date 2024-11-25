4366fee6ad31fc7eca900438f86078a8c34af521
AntonyNA <98645350+AntonyNA@users.noreply.github.com>
2024-11-25 21:09:46 +0530

    unauthorised auth using url is blocked

--- NeetMinds/src/app/app.routes.ts
+++ NeetMinds/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from '../components/sign-up/sign-up.component';
 import { HomeComponent } from '../components/home/home.component';
-
+import { authGuard } from '../auth.guard';
 export const routes: Routes = [
     {path: '', component:SignUpComponent},
-    {path: 'home', component:HomeComponent}
+    {path: 'home', component:HomeComponent, canActivate: [authGuard]},
+    { path: '**', redirectTo: '' }
 ];
---
+++ NeetMinds/src/auth.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export const authGuard: CanActivateFn = async (route, state) => {
+  const auth = inject(Auth);
+  const router = inject(Router);
+  const user = auth.currentUser;
+  if (user) {
+    console.log('User is authenticated:', user.uid);
+    return true;
+  } else {
+    console.log('User is not authenticated. Redirecting...');
+    router.navigate(['/'], { queryParams: { redirectTo: state.url } }); // Save the target URL
+    return false;
+  }
+};
